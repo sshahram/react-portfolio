704aec524e61fdb196cff9e6c8fb95ab67d190a7
sshahram <shirin.shahram23@gmail.com>
2021-06-12 12:05:27 -0700

    create Portfolio component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import { Switch, Route } from 'react-router-dom';
 import About from './components/About';
+import Portfolio from './components/Portfolio'
 
 function App() {
   return (
     <div>
       <Header>
         <Switch>
+          <Route path='/Portfolio' component={Portfolio} />
           <Route path='/' component={About} />
         </Switch>
       </Header>
