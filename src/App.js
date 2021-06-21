243d9dfff07192c254f493c28c1e5e363bef7d9c
sshahram <shirin.shahram23@gmail.com>
2021-06-20 21:06:58 -0700

    modify routing

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
     <>
       <Header className="page">
         <Switch>
-          <Route exact path='/About' component={About} />
-          <Route exact path='/' component={About} />
-          <Route exact path='/Portfolio' component={Portfolio} />
-          <Route exact path='/Contact' component={Contact} />
-          <Route exact path='/Resume' component={Resume} />
+          <Route exact path='/react-portfolio/About' component={About} />
+          <Route exact path='/react-portfolio/Portfolio' component={Portfolio} />
+          <Route exact path='/react-portfolio/Contact' component={Contact} />
+          <Route exact path='/react-portfolio/Resume' component={Resume} />
+          <Route exact path='/react-portfolio' component={About} />
         </Switch>
       </Header>
       <Footer></Footer>
