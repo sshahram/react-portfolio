55a8d45e2989e7cad0e898ba7b375492230f08d7
sshahram <shirin.shahram23@gmail.com>
2021-06-20 22:03:59 -0700

    modify environmental variables

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Footer from './components/Footer'
 
+require('dotenv').config();
+
 function App() {
   return (
     <>
       <Header className="page">
         <Switch>
-          <Route exact path={process.env.PUBLIC_URL + '/About'} component={About} />
-          <Route exact path={process.env.PUBLIC_URL + '/Portfolio'} component={Portfolio} />
-          <Route exact path={process.env.PUBLIC_URL + '/Contact'} component={Contact} />
-          <Route exact path={process.env.PUBLIC_URL + '/Resume'} component={Resume} />
-          <Route exact path={process.env.PUBLIC_URL + '/'} component={About} />
+          <Route path={process.env.PUBLIC_URL + '/About'} component={About} />
+          <Route path={process.env.PUBLIC_URL + '/Portfolio'} component={Portfolio} />
+          <Route path={process.env.PUBLIC_URL + '/Contact'} component={Contact} />
+          <Route path={process.env.PUBLIC_URL + '/Resume'} component={Resume} />
+          <Route path={process.env.PUBLIC_URL + '/'} component={About} />
         </Switch>
       </Header>
       <Footer></Footer>
