ae164338b6c24d59a94915342a6559c40494336f
sshahram <shirin.shahram23@gmail.com>
2021-06-20 22:08:02 -0700

    add exact to the routes

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
     <>
       <Header className="page">
         <Switch>
-          <Route path={process.env.PUBLIC_URL + '/About'} component={About} />
-          <Route path={process.env.PUBLIC_URL + '/Portfolio'} component={Portfolio} />
-          <Route path={process.env.PUBLIC_URL + '/Contact'} component={Contact} />
-          <Route path={process.env.PUBLIC_URL + '/Resume'} component={Resume} />
-          <Route path={process.env.PUBLIC_URL + '/'} component={About} />
+          <Route exact path={process.env.PUBLIC_URL + '/About'} component={About} />
+          <Route exact path={process.env.PUBLIC_URL + '/Portfolio'} component={Portfolio} />
+          <Route exact path={process.env.PUBLIC_URL + '/Contact'} component={Contact} />
+          <Route exact path={process.env.PUBLIC_URL + '/Resume'} component={Resume} />
+          <Route exact path={process.env.PUBLIC_URL + '/'} component={About} />
         </Switch>
       </Header>
       <Footer></Footer>
