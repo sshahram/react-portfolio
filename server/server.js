3dd56506f29882970be3a60dc8447f4e18cbea11
sshahram <shirin.shahram23@gmail.com>
2021-07-23 14:43:16 -0700

    connect the app to our defined PORT

--- server/server.js
+++ server/server.js
@@ -34,3 +34,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
   
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        // log where we can go to test our GQL API
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+});
