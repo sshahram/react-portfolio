bc99366715221b39f3dd58582e248e09d7c4689c
sshahram <shirin.shahram23@gmail.com>
2021-07-23 14:40:34 -0700

    serve up static assets in production environment

--- server/server.js
+++ server/server.js
@@ -24,3 +24,13 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// serve up static assets
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+  
