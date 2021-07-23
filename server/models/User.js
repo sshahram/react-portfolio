f1648561f30b3fb2dfdb54f227cb774df94e3dbc
sshahram <shirin.shahram23@gmail.com>
2021-07-23 11:31:23 -0700

    define user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: 'Please leave a message.',
+        minlength: 1
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
