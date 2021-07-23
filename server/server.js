ae98f1e113188486e0797c810cf1735ca8d0f07d
sshahram <shirin.shahram23@gmail.com>
2021-07-23 14:34:16 -0700

    create new Apollo Server and integrate it with Express application

--- server/server.js
+++ server/server.js
@@ -12,3 +12,15 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// create new Apollo Server and pass in our schema data
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
