73568205d05d3ae09cf636abe0c3f0034122b027
sshahram <shirin.shahram23@gmail.com>
2021-08-10 16:44:39 -0700

    change contact information style

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,13 +4,6 @@ import resume from "../../assets/Resume-Shirin Shahram.pdf";
 function ContactForm() {
     return (
         <section>
-            <div className="contact-info">
-                <h1 className="contact-title">Contact Info:</h1>
-                <a href="tel:818.271.6615" target="_blank" rel="noreferrer">Phone</a>
-                <a id="email" href="mailto:shirin.shahram23@gmail.com">shirin.shahram23@gmail.com</a>
-                <a href="https://github.com/sshahram" target="_blank" rel="noreferrer">GitHub</a>
-                <a href="https://www.linkedin.com/in/shirin-shahram" target="_blank" rel="noreferrer">LinkedIn</a>
-            </div>
             <h1 className="skill-header" id="myResume">My Resume:</h1>
             <div className="container"><a href={resume} id="resume" target="_blank" rel="noreferrer">Resume</a></div>
             <h1 className="skill-header">Technical Skills:</h1>
@@ -28,6 +21,13 @@ function ContactForm() {
                 <p>Sequelize, React, Handlebars.js, MERN</p>
                 <p>SPSS, Tableau, Visio, Minitab, AutoCAD</p>
                 <p>R, REST APIs, MongoDB, NoSQL</p> */}
+            </div>
+            <div className="contact-info">
+                <h1 className="skill-header">Contact Info:</h1>
+                <a href="tel:818.271.6615" target="_blank" rel="noreferrer">818-271-6615</a>
+                <a id="email" href="mailto:shirin.shahram23@gmail.com">shirin.shahram23@gmail.com</a>
+                <a href="https://github.com/sshahram" target="_blank" rel="noreferrer">GitHub</a>
+                <a href="https://www.linkedin.com/in/shirin-shahram" target="_blank" rel="noreferrer">LinkedIn</a>
             </div>
         </section>
     );
