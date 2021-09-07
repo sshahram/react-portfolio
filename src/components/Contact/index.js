427c7c5d173f1a78547ea7e0c03a4b00a6eeb8a1
sshahram <shirin.shahram23@gmail.com>
2021-09-07 13:42:35 -0700

    reorder icons for technical skills

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,13 +8,13 @@ function ContactForm() {
             <div className="container"><a href={resume} id="resume" target="_blank" rel="noreferrer">Resume</a></div>
             <h1 className="skill-header">Technical Skills:</h1>
             <div className="skills">
+                <div className="skill-block"><img src="https://img.icons8.com/color/144/000000/html-5--v1.png"alt="HTML"/> <p>HTML</p></div>
+                <div className="skill-block"><img src="https://img.icons8.com/color/144/000000/css3.png" alt="CSS"/> <p>CSS</p></div>
                 <div className="skill-block"><img src="https://img.icons8.com/color/144/000000/javascript--v2.png" alt="JavaScript"/> <p>JavaScript</p></div>
                 <div className="skill-block"><img src="https://img.icons8.com/color/144/000000/nodejs.png" alt="Node.js"/> <p>Node.js</p></div>
                 <div className="skill-block"><img src="https://img.icons8.com/color/144/000000/react-native.png" alt="React"/> <p>React</p></div>
                 <div className="skill-block"><img src="https://img.icons8.com/color/144/000000/mongodb.png" alt="MongoDB"/> <p>MongoDB</p></div>
                 <div className="skill-block"><img src="https://img.icons8.com/color/144/000000/mysql-logo.png" alt="MySQL"/> <p>MySQL</p></div>
-                <div className="skill-block"><img src="https://img.icons8.com/color/144/000000/html-5--v1.png"alt="HTML"/> <p>HTML</p></div>
-                <div className="skill-block"><img src="https://img.icons8.com/color/144/000000/css3.png" alt="CSS"/> <p>CSS</p></div>
                 <div className="skill-block"><img src="https://img.icons8.com/ios-filled/150/4a90e2/jquery.png" alt="jQuery"/> <p>jQuery</p></div>
                 {/* <p>Git, HTML5, CSS, JQuery, Javascript</p>
                 <p>Bootstrap, AJAX, Agile development</p>
